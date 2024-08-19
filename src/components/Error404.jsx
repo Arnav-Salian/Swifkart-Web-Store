716b95e0e2b9e4cf9e1e78c7f5fb02dbc4a70b85
Arnav Salian <asalian987@gmail.com>
2024-08-19 19:17:03 +0530

    Completed Mobile & Desktop Mega Footer, 404 Page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Error404.css'
 
 function Error404() {
   return (
     <>
-        <p>Error 404</p>
-        <p>Page Not Found</p>
-
-        <Link to="/"><button>Return to Homepage</button></Link>
+        <div className="error-page-wrapper">
+            <div className="error-container">
+                <p className='error-404'>Error 404</p>
+                <p className='page-not-found'>Page Not Found</p>
+                <Link to="/"><button>Return to Homepage</button></Link>
+            </div>
+            <div className="error-spacer"></div>
+        </div>
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import './Footer.css'
+import footerMap from '../assets/Footer-Map.png'
 
 function Footer() {
   return (
     <>
-    
+      <div className="mobile-wrapper">
+        <p className="credits">
+          © All Rights Reserved 2024 | Website developed by <a href="https://www.linkedin.com/in/arnavs27" target="_blank">Arnav Salian</a>
+        </p>
+      </div>
+
+      <div className="desktop-bg">
+        <div className="desktop-wrapper">
+          <div className="desktop-container">
+            <div className="footer-section">
+              <p className='footer-title'>Quick Links</p>
+              <ul>
+                <li>Terms and Conditions</li>
+                <li>Privacy Policy</li>
+                <li>Returns Policy</li>
+                <li>Documentation</li>
+                <li>Sitemap</li>
+              </ul>
+            </div>
+            <div className="footer-section">
+              <p className='footer-title'>Social Media</p>
+              <ul>
+                <li>Twitter X</li>
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>YouTube</li>
+                <li>LinkedIn</li>
+              </ul>
+            </div>
+            <div className="footer-section">
+              <p className='footer-title'>Find Us</p>
+              <p className='footer-address'>
+                The University of Leicester, <br />
+                University Road, <br />
+                Leicester, <br />
+                LE1 7RH, <br />
+                United Kingdom <br />
+                <br />
+                <a href="" target='_blank'>View on Google Maps</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
