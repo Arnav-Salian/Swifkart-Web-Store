ccf14743957cf5f45f116ba9c7410deb882377ce
Arnav Salian <asalian987@gmail.com>
2024-08-20 17:12:11 +0530

    Styled Home - Product Display

--- backend/products.js
+++ backend/products.js
@@ -1,24 +1,24 @@
 const products = [
     {
         id:1,
-        name: "Red Bull Energy Drink",
+        name: "Red Bull Drink",
         desc: "Regular",
-        price: 3.50,
-        image: "/assets/redbull-reg.webp",
+        price: 3.55,
+        image: "https://www.redbull.com/energydrink/v1/resources/contentful/images/lcr8qbvxj7mh/3r9LNEQkKc4Qe6YaJnNBJ1/105d7f426869b046ba6984ab995791c9/IN_RBED_250_Single_Unit_close_ambient_ORIGINAL_DRES__1_.png?h=453&fm=webp&q=90",
     },
     {
         id:2,
         name: "Red Bull Sugarfree",
         desc: "Diet",
         price: 2.99,
-        image: "/assets/redbul-sugarfree.webp",
+        image: "https://www.redbull.com/energydrink/v1/resources/contentful/images/lcr8qbvxj7mh/5yJ5aAgTkSl24TXP9IFA7l/933582c6cabc953a9d0001a23bb3dcfc/IN_RBSF_250_Single_Unit_close_ambient_ORIGINAL_DRES__1_.png?h=453&fm=webp&q=90",
     },
     {
         id:3,
         name: "Red Bull Summer",
         desc: "Special Edition",
         price: 3.27,
-        image: "/assets/redbull-summer.webp",
+        image: "https://www.redbull.com/energydrink/v1/resources/contentful/images/lcr8qbvxj7mh/3aAXegbkkF6vjeLCmYxsxk/fef933bdb7f29eaef6d5867ed804fac4/DRES_IN_SUMMER-Coconut-250ml_ambient_front_redbullcom.png?h=453&fm=webp&q=90",
     }
 ];
 
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import { useGetAllProductsQuery } from '../features/productsApi';
+import { productsApi, useGetAllProductsQuery } from '../features/productsApi';
+import './Home.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
+
   const {data, error, isLoading} = useGetAllProductsQuery()
   return (
     <>
-      <p>Home</p>
+      <div className="home-wrapper">
+        <div className="home-container">
+            {isLoading ? <p>Loading...</p> : error ? 
+            <p>An error occured...</p> : 
+            <>
+            <div className="products">
+              {data?.map(product => <div key={product.id} className="product">
+                  
+                  <img src={product.image} alt="" draggable="false"/>
+                  <p className='product-name'>{product.name}</p>
+                  <div className="details">
+                    <div className="tags">
+                      <span className='desc'>{product.desc}</span>
+                      <span className='size'>300ml</span>
+                    </div>
+                    <span className="price">£{product.price}</span>
+                  </div>
+                  <button><FontAwesomeIcon className='add-cart-icon' icon={faCartShopping}/>Add To Cart</button>
+                </div> )}
+            </div>
+            </> }
+          </div>
+      </div>
     </>
   )
 }
